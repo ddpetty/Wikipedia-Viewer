673a6bce6339875cf8d4df92d2357fc5b46d850b
ddpetty <dionne.petty@gmail.com>
2017-10-18 16:19:25 -0400

    Implementing extra features.

--- WikiViewer.js
+++ WikiViewer.js
@@ -1,11 +1,15 @@
 $(function() {
  /* https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages
  |extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=dionne&callback=? */
- //Todos: Hover affects. Media queries. Link random article button. Keycode enter button for searches. Implement continuous searches.
+ //Todos: Hover affects. Media queries. Find way to remove ajax before new search  Make this functional in IE 
+ 	$('#search-bar').keydown(function(event) {
+            if (event.keyCode == 13) {  //  enter button pressed
+                 $('#search-button').click(); //run click function
+            }
+        })
 
  	$('#search-button').click(function(){
  		const searchBar = $('#search-bar').val();
-
  		$.ajax({
 			type: 'GET',
 			url: `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages
@@ -16,30 +20,25 @@ $(function() {
 			//console.log(data.query.pages);
 			let articles = data.query.pages;
 
-
-			for (let key in articles) {
-				let title = articles[key].title,
-				description = articles[key].extract,
-				pageid = articles[key].pageid,
-				link = "https://en.wikipedia.org/?curid=" +pageid;
-
-			$('.search-results').append('<div class="result-title">'+ '<a href="'+link+'" target="_blank">'+title+'</a>'
-				+ '</br>' + '<div class="result-p">' +description+ '</div></div>');
+			//Looping through json
+				for (let key in articles) {
+					const title = articles[key].title,
+					description = articles[key].extract,
+					pageid = articles[key].pageid,
+					link = "https://en.wikipedia.org/?curid=" +pageid;
 				
-			}
+				$('.search-results').append('<div class="result-title">'+ '<a href="'+link+'" target="_blank">'+title+'</a>'
+					+ '</br>' + '<div class="result-p">' +description+ '</div></div>');	
+				}
+
+			$('#search-bar').val('');
+			
 
 			},
 			error: function(error) {
 				alert("This is an error.");
 			}
- 		
  		})
  	});
 
-
-
- 		
- 		
- 
-//press enter on keyboard to search
 });
