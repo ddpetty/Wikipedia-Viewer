1f0901e7e3afe6bd1737027f250d5452bc2182a6
ddpetty <dionne.petty@gmail.com>
2017-10-18 19:30:55 -0400

    Implemented extra features, media queries, etc.

--- WikiViewer.js
+++ WikiViewer.js
@@ -1,13 +1,8 @@
 $(function() {
  /* https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages
  |extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=dionne&callback=? */
- //Todos: Hover affects. Media queries. Find way to remove ajax before new search  Make this functional in IE 
- 	$('#search-bar').keydown(function(event) {
-            if (event.keyCode == 13) {  //  enter button pressed
-                 $('#search-button').click(); //run click function
-            }
-        })
-
+ //Todos: Hover affects. Make this functional in IE. 
+ 	
  	$('#search-button').click(function(){
  		const searchBar = $('#search-bar').val();
  		$.ajax({
@@ -18,27 +13,32 @@ $(function() {
 			dataType: 'json',
 			success: function(data) {
 			//console.log(data.query.pages);
-			let articles = data.query.pages;
 
+			//Start off with empty slate for each request
+			$('.search-results').empty();
+			const articles = data.query.pages;
 			//Looping through json
 				for (let key in articles) {
 					const title = articles[key].title,
 					description = articles[key].extract,
 					pageid = articles[key].pageid,
 					link = "https://en.wikipedia.org/?curid=" +pageid;
-				
-				$('.search-results').append('<div class="result-title">'+ '<a href="'+link+'" target="_blank">'+title+'</a>'
+
+					$('.search-results').append('<div class="result-title">'+ '<a href="'+link+'" target="_blank">'+title+'</a>'
 					+ '</br>' + '<div class="result-p">' +description+ '</div></div>');	
-				}
-
-			$('#search-bar').val('');
-			
-
+				}	
 			},
 			error: function(error) {
 				alert("This is an error.");
 			}
  		})
  	});
+
+
+ 	$('#search-bar').keydown(function(event) {
+            if (event.keyCode == 13) {  //  enter button pressed
+                 $('#search-button').click(); //run click function
+            }
+        })
 
 });
