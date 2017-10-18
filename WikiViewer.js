e8822b4666e35e1cf803e25cf1d70c5babd8d55f
ddpetty <dionne.petty@gmail.com>
2017-10-17 20:58:16 -0400

    Structuring data on UI.

--- WikiViewer.js
+++ WikiViewer.js
@@ -1,33 +1,48 @@
 $(function() {
- //https://en.wikipedia.org/w/api.php?action=opensearch&search=dionne&callback=?"
+ /* https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages
+ |extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=dionne&callback=? */
 
  	$('#search-button').click(function(){
- 		let searchBar = $('#search-bar').val();
+ 		const searchBar = $('#search-bar').val();
 
  		$.ajax({
 			type: 'GET',
-			url: `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchBar}&format=json&callback=?`,
+			url: `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages
+			|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${searchBar}&callback=?`,
 			async: false,
 			dataType: 'json',
 			success: function(data) {
-			//console.log(data[2][1]);
+			//console.log(data.query.pages);
+			let articles = data.query.pages;
 
-			for(let i = 0; i <= 10; i++) {
-				$('.search-results').append(data[3][i]);	
+
+			for (let key in articles) {
+			let title = articles[key].title,
+				description = articles[key].extract;
+			$('.search-results').append('<div class="result-title">'+ '<a href="#">'+title+'<a>'+ '</br>' +description+ '</div>');
+				
 			}
 
+			
 
+			
 
+	
+			
+			
 
 			},
 			error: function(error) {
 				alert("This is an error.");
 			}
-
-
-
- 		})
  		
+ 		})
  	});
+
+
+
+ 		
+ 		
+ 
 //press enter on keyboard to search
 });
